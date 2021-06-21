e9c291b16519d5ea29cfac46e380d3039efc8c13
EvanBalster <evan@interactopia.com>
2021-06-20 18:04:38 -0700

    Disable life_lock's copy constructor (which can be disastrous)

--- include/life_lock.h
+++ include/life_lock.h
@@ -227,6 +227,12 @@ namespace edb
 		};
 		shared_ref _ref = shared_ref();
 		std::atomic_flag &_lock() noexcept    {return reinterpret_cast<std::atomic_flag&>(_ref);}
+		
+		// Prevent implicit copying of life_lock.
+		//    life_lock holds a shared reference and thus blocks destruction of its clones.
+		//    TODO consider an explicit cloning method for careful multithreaded use.
+		life_lock           (const life_lock &o) noexcept = delete;
+		life_lock& operator=(const life_lock &o) noexcept = delete;
 	};
 
 
